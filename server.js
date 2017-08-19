7de045fb96adb9b134514b60fd1f758ae1091423
Anubhav Mittra <anubhavmittra19@gmail.com>
2017-08-19 21:41:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/:articleName',function(req,res){
     //articleName== article-0ne
     //articles[articleName]=={}content object for article one
