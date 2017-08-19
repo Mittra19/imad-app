58136fee923fbb05862b333eb2ea3ecf8c3a5378
Anubhav Mittra <anubhavmittra19@gmail.com>
2017-08-19 21:54:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+
+var element= document.getElementById('main-txt');
+element.innerHTML= 'New Text';
+
+var img= document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft='150px';
+};
